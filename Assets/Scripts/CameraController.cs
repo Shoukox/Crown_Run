042354ce07607fc806e30fa4089f015cf4e56018
Ахрорходжаев Шахзод <shachzod2004@gmail.com>
2021-04-19 00:26:07 +0500

    .

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,8 +6,18 @@ public class CameraController : MonoBehaviour
 {
     public GameObject target;
     public float CameraSpeed = 2f;
+    public float linkGrenze;
+    public float rechtGrenze;
+    public float untenGrenze;
+    public float obenGrenze;
+
+    public void KameraStop()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, linkGrenze, rechtGrenze),Mathf.Clamp(transform.position.y, obenGrenze, untenGrenze),transform.position.z);
+    }
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, -10f) , Time.deltaTime * CameraSpeed);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, -10f), Time.deltaTime * CameraSpeed);
+        KameraStop();
     }
 }
